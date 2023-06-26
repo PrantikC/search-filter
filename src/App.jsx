78a9a45bea471389f520659777625800cf0d2bdb
PrantikC <prantik.chowdhury.25696@gmail.com>
2023-06-26 21:05:21 +0530

    Dynamic List

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,45 @@
-import React, { useState } from 'react'
-import { Users } from './Users'
+import React, { useEffect, useState } from 'react'
 import "./app.css"
 
 const App = () => {
-  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+  const [filterText, setFilterText] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await fetch("https://randomuser.me/api/?results=100");
+        const resJson = await response.json();
+        setData(resJson.results);
+        setFilteredData(resJson.results);
+      } catch(error) {
+        console.error("Couldn't fetch data", error);
+      }
+    }
+    fetchData();
+  }, [])
+
+  const filterChangeHandler = (e) => {
+    setFilterText(e.target.value);
+    filteredDataHandler(e.target.value);
+  }
+
+  const filteredDataHandler = (text) => {
+    const filtered = filteredData.filter((data) => data.name.first.toLowerCase().includes(text.toLowerCase()));
+    setFilteredData(filtered);
+  }
+
+  const resetHandler = () => {
+    setFilteredData([]);
+  }
 
   return (
     <div>
-      <input type="text" placeholder='Search...' className='search' onChange={(e) => setInput(e.target.value)} value={input}/>
+      <input type="text" placeholder='Search...' className='search' onChange={filterChangeHandler} value={filterText}/>
+      <button onClick={resetHandler}>Reset</button>
       <ul className='list'>
-        {Users.filter((data) => data.first_name.toLowerCase().includes(input)).map((data) => <li className='listItem'>{data.first_name}</li>)}
+        {filteredData.map((user) => <li className='listItem' key={user.login.uuid}>{user.name.first}</li>)}
       </ul>
     </div>
   )
